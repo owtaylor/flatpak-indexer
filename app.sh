2179ee41ba134e2c79e80b1b4146890e1f7ed726
Owen W. Taylor <otaylor@fishsoup.net>
2019-11-08 10:39:12 -0500

    app.sh: fix creation of output directories

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-[ -d $OUTPUT_DIR/icons ] || mkdir -p 0755 $OUTPUT_DIR/icons
-[ -d $OUTPUT_DIR/index ] || mkdir -p 0755 $OUTPUT_DIR/index
+[ -d $OUTPUT_DIR/icons ] || mkdir -p -m 0755 $OUTPUT_DIR/icons
+[ -d $OUTPUT_DIR/index ] || mkdir -p -m 0755 $OUTPUT_DIR/index
 
 exec flatpak-indexer -v -c /etc/flatpak-indexer/config.yaml daemon
