12f6fe22c1828da587660099299f4f8c689dc960
Owen W. Taylor <otaylor@fishsoup.net>
2019-11-08 10:21:22 -0500

    app.sh: Create the output directories if missing

--- app.sh
+++ app.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+[ -d $OUTPUT_DIR/icons ] || mkdir -p 0755 $OUTPUT_DIR/icons
+[ -d $OUTPUT_DIR/index ] || mkdir -p 0755 $OUTPUT_DIR/index
+
 exec flatpak-indexer -v -c /etc/flatpak-indexer/config.yaml daemon
