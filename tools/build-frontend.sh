6fdd0dc4648ba00ad0a2e13c90df3f7da02db706
Owen W. Taylor <otaylor@fishsoup.net>
2020-03-13 14:00:41 -0400

    Redo frontend configuration
     - Switch from s2i build to a direct Dockerfile build
     - Switch from dropping in a configuration conf to replacing
       httpd.conf and ssl.conf entirely - this allows seeing better
       how the server is configured.
     - Add custom error pages for 400/404 (with text fallbacks if a
       Accept header doesn't explicitly request text/html)
     - Enable mod_security with the OWASP Core Rule Set

--- tools/build-frontend.sh
+++ tools/build-frontend.sh
@@ -2,15 +2,5 @@
 
 set -ex
 
-origin=$(cd $(dirname $0)/.. && pwd)
-
-work=$(mktemp -d)
-cleanup() {
-    rm -rf $work
-}
-trap cleanup EXIT
-
-set -x
-
-s2i build --copy --as-dockerfile=$work/Dockerfile --context-dir=frontend $origin registry.redhat.io/rhscl/httpd-24-rhel7 flatpak-indexer-frontend
-podman build $work -t flatpak-indexer-frontend
+context=$(cd $(dirname $0)/.. && pwd)/frontend
+exec podman build $context -t flatpak-indexer-frontend
