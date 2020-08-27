2c29c08f08b45f8967e32b86496aaa2a7868fc21
Owen W. Taylor <otaylor@fishsoup.net>
2020-08-27 11:30:10 -0400

    run-redis.sh: switch to docker.io/library/redis:5
    Use the upstream Redis image rather than a RHEL image to simplify
    working on this codebase without a RHEL subscription. Stick to
    version 5 for compatibility with what is available in RHEL 8.

--- tools/run-redis.sh
+++ tools/run-redis.sh
@@ -6,12 +6,12 @@ topdir=$(cd $(dirname $0)/.. && pwd)
 
 podman run \
         --rm --user 0:0 \
-        -v $topdir/work/redis-data:/var/lib/redis/data:z \
-        registry.redhat.io/rhel8/redis-5 \
-        chown redis:redis /var/lib/redis/data
+        -v $topdir/work/redis-data:/data:z \
+        docker.io/library/redis:5 \
+        chown redis:redis /data
 
 exec podman run \
         --name=flatpak-redis --rm \
         -p 6379:6379 \
-        -v $topdir/work/redis-data:/var/lib/redis/data:z \
-        registry.redhat.io/rhel8/redis-5
+        -v $topdir/work/redis-data:/data:z \
+        docker.io/library/redis:5
