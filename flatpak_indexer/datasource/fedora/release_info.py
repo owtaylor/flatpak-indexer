665c80047a7c15145faa8d77b60fadd2feebb1e1
Owen W. Taylor <otaylor@fishsoup.net>
2021-03-29 09:59:48 -0400

    release_info.py: Update for F34 => branched
    Also update the git branch for rawhide (used for rpms/*) to 'rawhide'.

--- flatpak_indexer/datasource/fedora/release_info.py
+++ flatpak_indexer/datasource/fedora/release_info.py
@@ -18,13 +18,14 @@ class Release:
 
 
 releases = [
-    Release(name='F26', branch='f26',    tag='f26', status=ReleaseStatus.EOL),
-    Release(name='F27', branch='f27',    tag='f27', status=ReleaseStatus.EOL),
-    Release(name='F28', branch='f28',    tag='f28', status=ReleaseStatus.EOL),
-    Release(name='F29', branch='f29',    tag='f29', status=ReleaseStatus.EOL),
-    Release(name='F30', branch='f30',    tag='f30', status=ReleaseStatus.EOL),
-    Release(name='F31', branch='f31',    tag='f31', status=ReleaseStatus.EOL),
-    Release(name='F32', branch='f32',    tag='f32', status=ReleaseStatus.GA),
-    Release(name='F33', branch='f33',    tag='f33', status=ReleaseStatus.GA),
-    Release(name='F34', branch='master', tag='f34', status=ReleaseStatus.RAWHIDE),
+    Release(name='F26', branch='f26',     tag='f26', status=ReleaseStatus.EOL),
+    Release(name='F27', branch='f27',     tag='f27', status=ReleaseStatus.EOL),
+    Release(name='F28', branch='f28',     tag='f28', status=ReleaseStatus.EOL),
+    Release(name='F29', branch='f29',     tag='f29', status=ReleaseStatus.EOL),
+    Release(name='F30', branch='f30',     tag='f30', status=ReleaseStatus.EOL),
+    Release(name='F31', branch='f31',     tag='f31', status=ReleaseStatus.EOL),
+    Release(name='F32', branch='f32',     tag='f32', status=ReleaseStatus.GA),
+    Release(name='F33', branch='f33',     tag='f33', status=ReleaseStatus.GA),
+    Release(name='F34', branch='f34',     tag='f34', status=ReleaseStatus.BRANCHED),
+    Release(name='F34', branch='rawhide', tag='f35', status=ReleaseStatus.RAWHIDE),
 ]
