9edd8348f2abf8a8ff21d99427946224ea71f15c
Owen W. Taylor <otaylor@fishsoup.net>
2021-10-07 14:32:46 -0400

    indexer.py: Implement a simple build cache
    Add a class to cache repeated lookups of module/image builds.

--- flatpak_indexer/indexer.py
+++ flatpak_indexer/indexer.py
@@ -47,6 +47,35 @@ class IconStore(object):
         return uri_for_digest(self.icons_uri, digest, ".png")
 
 
+class BuildCache:
+    image_builds: Dict[str, ImageBuildModel]
+    module_builds: Dict[str, ModuleBuildModel]
+
+    def __init__(self, global_config: Config):
+        self.koji_session = get_koji_session(global_config)
+        self.redis_client = get_redis_client(global_config)
+        self.image_builds = {}
+        self.module_builds = {}
+
+    def get_image_build(self, nvr: str):
+        image_build = self.image_builds.get(nvr)
+        if image_build:
+            return image_build
+
+        image_build = query_image_build(self.koji_session, self.redis_client, nvr)
+        self.image_builds[nvr] = image_build
+        return image_build
+
+    def get_module_build(self, nvr: str):
+        module_build = self.module_builds.get(nvr)
+        if module_build:
+            return module_build
+
+        module_build = query_module_build(self.koji_session, self.redis_client, nvr)
+        self.module_builds[nvr] = module_build
+        return module_build
+
+
 class IndexWriter:
     def __init__(
         self, conf: IndexConfig, registry_config: RegistryConfig,
--- tests/test_indexer.py
+++ tests/test_indexer.py
@@ -7,7 +7,7 @@ import yaml
 
 from flatpak_indexer.cleaner import Cleaner
 from flatpak_indexer.datasource import load_updaters
-from flatpak_indexer.indexer import Indexer
+from flatpak_indexer.indexer import BuildCache, Indexer
 from flatpak_indexer.models import RegistryModel
 from .bodhi import mock_bodhi
 from .fedora_messaging import mock_fedora_messaging
@@ -257,3 +257,22 @@ def test_indexer_fedora(connection_mock, tmp_path):
 
     baobab_image = baobab_repository['Images'][0]
     assert baobab_image['Labels']['org.flatpak.ref'] == 'app/org.gnome.Baobab/x86_64/stable'
+
+
+@mock_koji
+@mock_redis
+def test_build_cache(tmp_path):
+    os.environ["OUTPUT_DIR"] = str(tmp_path)
+
+    config = get_config(tmp_path, FEDORA_CONFIG)
+    build_cache = BuildCache(config)
+
+    image_a = build_cache.get_image_build("baobab-master-3220200331145937.2")
+    image_b = build_cache.get_image_build("baobab-master-3220200331145937.2")
+
+    assert image_b is image_a
+
+    module_a = build_cache.get_module_build('baobab-master-3220200331145937')
+    module_b = build_cache.get_module_build('baobab-master-3220200331145937')
+
+    assert module_b is module_a
