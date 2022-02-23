bb9e3f9f92eff3838c1b5ba77bf2199d50ae161c
Owen W. Taylor <otaylor@fishsoup.net>
2022-02-23 17:04:44 -0500

    app.sh tools/test.sh: Unset REQUESTS_CA_BUNDLE and GIT_SSL_CAINFO
    These variables are set at build time pointing to a CA bundle that
    is then removed - unset them for running tests and the app.
    
    (It would be nice to avoid leaking them into the Dockerfile environment
    at all, but that isn't easily possible with s2i and the openshift
    sourceStrategy.)

--- app.sh
+++ app.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# These is set to build-time specific location when building the Red Hat-internal
+# image; unset them to avoid breaking things at runtime.
+unset REQUESTS_CA_BUNDLE
+unset GIT_SSL_CAINFO
+
 for d in deltas icons ; do
     [ -d $OUTPUT_DIR/$d ] || mkdir -p -m 0755 $OUTPUT_DIR/$d
 done
