641b9bfd893e9a3e3bc13ee89aefe6175dd17d96
Owen W. Taylor <otaylor@fishsoup.net>
2022-04-05 10:13:30 -0400

    koji_utils: add a KojiConfig base class
    Allow calling get_koji_session() without a full Config object.

--- flatpak_indexer/koji_utils.py
+++ flatpak_indexer/koji_utils.py
@@ -1,7 +1,14 @@
 import koji
 
 
-def get_koji_session(config):
+from .base_config import BaseConfig
+
+
+class KojiConfig(BaseConfig):
+    koji_config: str
+
+
+def get_koji_session(config: KojiConfig):
     options = koji.read_config(profile_name=config.koji_config)
     koji_session_opts = koji.grab_session_options(options)
     return koji.ClientSession(options['server'], koji_session_opts)
---
+++ tests/test_koji_utils.py
@@ -0,0 +1,15 @@
+from flatpak_indexer.koji_utils import get_koji_session, KojiConfig
+
+from .koji import mock_koji
+
+
+CONFIG = """
+koji_config: fedora
+"""
+
+
+@mock_koji
+def test_get_koji_session():
+    config = KojiConfig.from_str(CONFIG)
+    session = get_koji_session(config)
+    assert session.getPackageID("eog") == 303
