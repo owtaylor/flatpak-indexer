4b0e63e509fc216c95550f67bcda26591f18e10c
Owen W. Taylor <otaylor@fishsoup.net>
2022-05-23 09:35:31 -0400

    release_info.py: Update for F36 release

--- flatpak_indexer/release_info.py
+++ flatpak_indexer/release_info.py
@@ -26,7 +26,8 @@ releases = [
     Release(name='F31', branch='f31',     tag='f31', status=ReleaseStatus.EOL),
     Release(name='F32', branch='f32',     tag='f32', status=ReleaseStatus.EOL),
     Release(name='F33', branch='f33',     tag='f33', status=ReleaseStatus.EOL),
-    Release(name='F34', branch='f34',     tag='f34', status=ReleaseStatus.GA),
+    Release(name='F34', branch='f34',     tag='f34', status=ReleaseStatus.EOL),
     Release(name='F35', branch='f35',     tag='f35', status=ReleaseStatus.GA),
-    Release(name='F36', branch='rawhide', tag='f36', status=ReleaseStatus.BRANCHED),
+    Release(name='F36', branch='f36',     tag='f36', status=ReleaseStatus.GA),
+    Release(name='F37', branch='rawhide', tag='f37', status=ReleaseStatus.RAWHIDE),
 ]
