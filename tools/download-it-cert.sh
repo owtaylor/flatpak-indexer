160dae353ceb4995f3104067cb4708b98dcbf716
Owen W. Taylor <otaylor@fishsoup.net>
2024-09-24 10:45:54 -0400

    download-it-cert.sh: Update URL for RH IT ROot CA

--- tools/download-it-cert.sh
+++ tools/download-it-cert.sh
@@ -2,5 +2,5 @@
 
 topdir=$(cd $(dirname $0)/.. && pwd)
 
-curl -o $topdir/flatpak_indexer/certs/RH-IT-Root-CA.crt https://password.corp.redhat.com/RH-IT-Root-CA.crt
+curl -o $topdir/flatpak_indexer/certs/RH-IT-Root-CA.crt https://certs.corp.redhat.com/certs/2022-IT-Root-CA.pem
 
