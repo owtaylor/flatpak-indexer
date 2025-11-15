db85d65ba51132d39438f7d009abe6d860621ea6
Owen W. Taylor <otaylor@fishsoup.net>
2025-11-15 17:23:05 -0500

    test/redis.py: Add precise type annotations
    Use typing.ParamSpec for precise annotations for the
    mock_redis decorator.

--- flatpak_indexer/test/redis.py
+++ flatpak_indexer/test/redis.py
@@ -1,4 +1,5 @@
-from functools import partial, wraps
+from functools import wraps
+from typing import Callable, ParamSpec, TypeVar, overload
 from unittest.mock import patch
 
 import fakeredis
@@ -8,14 +9,37 @@ def make_redis_client():
     return fakeredis.FakeStrictRedis()
 
 
-def mock_redis(f=None, expect_url=None):
+P = ParamSpec("P")
+R = TypeVar("R")
+
+
+@overload
+def mock_redis(f: Callable[P, R], expect_url: str | None = None) -> Callable[P, R]: ...
+
+
+@overload
+def mock_redis(
+    f: None = None, expect_url: str | None = None
+) -> Callable[[Callable[P, R]], Callable[P, R]]: ...
+
+
+def mock_redis(
+    f: Callable[P, R] | None = None, expect_url=None
+) -> Callable[P, R] | Callable[[Callable[P, R]], Callable[P, R]]:
     if f is None:
         # Called with arguments, return a version with the arguments bound
         # to be used as the actual decorator.
-        return partial(mock_redis, expect_url=expect_url)
+        def decorator(
+            f: Callable[P, R],
+        ) -> Callable[P, R]:
+            return mock_redis(f, expect_url=expect_url)
+
+        return decorator
 
     @wraps(f)
-    def wrapper(*args, **kwargs):
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        assert f is not None  # pyrefly needs this
+
         server = fakeredis.FakeServer()
 
         def from_url(url, **kwargs):
